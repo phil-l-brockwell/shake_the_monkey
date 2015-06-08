b896a422f0e6abf7774026e8ed00b0a1233ee0fe
Phil Brockwell <nineteen40eight@gmail.com>
2015-06-08 19:53:47 +0100

    added route for new search

--- server.rb
+++ server.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
+require_relative './lib/shake_the_monkey.rb'
+require_relative './lib/complete_works.rb'
+
+# @test = ShakeTheMonkey.new(SWORDS)
+
 get '/' do
+  @test = 'empty'
+  erb :index
+end
+
+post '/new_search' do
+  @test = params[:search]
   erb :index
 end
