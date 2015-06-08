b0c909222baabe2e8d7f0c43c7ea0fb8589ff310
Phil Brockwell <nineteen40eight@gmail.com>
2015-06-08 19:07:13 +0100

    added gemfile and sinatra

--- spec/shake_the_monkey_spec.rb
+++ spec/shake_the_monkey_spec.rb
@@ -1,11 +1,11 @@
 require 'shake_the_monkey'
 
 describe 'Shake_the_monkey' do
-  let(:test_string)      { 'test 1'                          }
-  let(:shake_the_monkey) { ShakeTheMonkey.new(test_string)   }
+  let(:test_string)      { 'test 1'                        }
+  let(:shake_the_monkey) { ShakeTheMonkey.new(test_string) }
 
   it 'initialises with an array of words' do
-    expect(shake_the_monkey.words).to eq(%w[test 1])
+    expect(shake_the_monkey.words).to eq(%w(test 1))
   end
 
   it 'initialises with the amount of blank spaces' do
@@ -17,46 +17,43 @@ describe 'Shake_the_monkey' do
   end
 
   it 'can add the blank spaces into the array of words' do
-    expect(shake_the_monkey.add_blanks(%w[test 1]))
+    expect(shake_the_monkey.add_blanks(%w(test 1)))
       .to eq(['test', '1', ' '])
   end
 
   it 'can split an array of words into an array of chars' do
     words_array = %w(words array)
     expect(shake_the_monkey.split_into_chars(words_array))
-      .to eq(%w[w o r d s a r r a y])
+      .to eq(%w(w o r d s a r r a y))
   end
 
   it 'can shuffle an array of chars' do
-    chars_array = %w[c h a r s a r r a y]
+    chars_array = %w(c h a r s a r r a y)
     allow(shake_the_monkey).to receive(:shuffle_chars).with(chars_array)
-      .and_return(%w[h c a r s a r r a y])
+      .and_return(%w(h c a r s a r r a y))
     expect(shake_the_monkey.shuffle_chars(chars_array))
-      .to eq(%w[h c a r s a r r a y])
+      .to eq(%w(h c a r s a r r a y))
   end
 
   it 'can join an array of chars into an array of string' do
     chars_array = ['h', 'c', ' ', ' ', 's', 'a', 'r', ' ', 'a', 'y']
     expect(shake_the_monkey.convert_to_strings(chars_array))
-      .to eq(%w[hc sar ay])
+      .to eq(%w(hc sar ay))
   end
 
   it 'can return a shuffled version of the words array' do
     allow(shake_the_monkey).to receive(:shuffle_words)
-      .and_return(%w[t1 ste])
-    expect(shake_the_monkey.shuffle_words)
-      .to eq(%w[t1 ste])
+      .and_return(%w(t1 ste))
+    expect(shake_the_monkey.shuffle_words).to eq(%w(t1 ste))
   end
 
   it 'can remove the special chars from a string' do
     mixed_string = 'ab@/c'
-    expect(shake_the_monkey.normalise(mixed_string))
-      .to eq('abc')
+    expect(shake_the_monkey.normalise(mixed_string)).to eq('abc')
   end
 
   it 'can downcase all the chars in a string' do
     mixed_case_string = 'AbcD'
-    expect(shake_the_monkey.normalise(mixed_case_string))
-      .to eq('abcd')
+    expect(shake_the_monkey.normalise(mixed_case_string)).to eq('abcd')
   end
 end
